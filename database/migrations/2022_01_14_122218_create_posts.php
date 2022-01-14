f6295aa270ea951c64518525ee7f315fcd6240ed
NikitaKirin <kirinnikita1406@gmail.com>
2022-01-14 12:52:50 +0500

    подготовлены миграции для основных сущностей

---
+++ database/migrations/2022_01_14_122218_create_posts.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePosts extends Migration
+{
+    public function up() {
+        Schema::create('posts', function ( Blueprint $table ) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->text('description');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down() {
+        Schema::dropIfExists('posts');
+    }
+}
