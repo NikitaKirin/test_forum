c78a7be93c90fbee4ba19b55967e19c41cc502b6
NikitaKirin <kirinnikita1406@gmail.com>
2022-01-14 14:00:41 +0500

    Создан header для всех страниц форума

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.app')
 @section('title', __('Тестовый форум'))
 @section('main')
-    <form>
-        @csrf
-        <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
-        </div>
-        <div class="mb-3">
-            <label for="password" class="form-label">Пароль</label>
-            <input type="password" class="form-control" id="password">
-        </div>
-        <button type="submit" class="btn btn-primary">Войти</button>
-    </form>
+    <div style="max-width: 400px; margin: 15px auto 0 auto;">
+        <form>
+            @csrf
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Пароль</label>
+                <input type="password" class="form-control" id="password">
+            </div>
+            <button type="submit" class="btn btn-primary">Войти</button>
+        </form>
+    </div>
 @endsection
